2768610945e1ef3467cc9e3b3dbbbf35a6e585e4
klmpk18 <74642187+klmpk18@users.noreply.github.com>
2021-01-24 10:54:09 +0700

    Create Interface3.php

---
+++ Interface3.php
@@ -0,0 +1,8 @@
+<?php
+Interface Interface {
+	public function Method1();
+	public function Method2($name, $age);
+	public function Method3() : int;
+}
+class Class implements Interface { /* ... */ }
+?>
